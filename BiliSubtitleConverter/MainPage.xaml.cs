8f1c3fe1910a61de289edab721e21295c03dc3ba
jimmy <jimmy@MAINDESKTOP>
2023-11-25 09:37:02 +0800

    添加项目文件。

---
+++ BiliSubtitleConverter/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Newtonsoft.Json;
+using BiliSubtitleConverter.Models;
+
+namespace BiliSubtitleConverter
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string jsonText = InputJsonTextBox.Text;
+            if(jsonText != "")
+            {
+                var dynObj = JsonConvert.DeserializeObject<ObservableCollection<Lines>>(jsonText);
+                int i = 0;
+                string convertedSrtString = "";
+                foreach (var line in dynObj)
+                {
+                    string startTime;
+                    string endTime;
+                    startTime = SecondsToTime(line.from);
+                    endTime = SecondsToTime(line.to);
+                    convertedSrtString += ((i + 1) + "\r\n");
+                    convertedSrtString += ((startTime) + " --> " + (endTime) + "\r\n");
+                    convertedSrtString += ((line.content) + "\r\n\r\n");
+                    ++i;
+                }
+                OutputJsonTextBox.Text = convertedSrtString;
+            }
+        }
+
+        public string SecondsToTime(double seconds)
+        {
+            TimeSpan time = TimeSpan.FromSeconds(seconds);
+            return time.ToString(@"hh\:mm\:ss\,fff");
+        }
+    }
+}
---
+++ BiliSubtitleConverter/Models/Lines.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiliSubtitleConverter.Models
+{
+    public class Lines
+    {
+        public string content { get; set; }
+        public int location { get; set; }
+        public double from { get; set; }
+        public double to { get; set; }
+    }
+}
